c735b9292ddf70e75478e55734fa5f30fa008eb6
mohabrd <bezrhoudimohamed@gmail.com>
2023-01-03 16:49:51 +0100

    exo2

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,28 @@
 //EXO 1
-let moha = {
-    nom : 'brd',
-    prenom : 'moha',
-    age : 19,
-    taille : '1,70m',
+// let moha = {
+//     nom : 'brd',
+//     prenom : 'moha',
+//     age : 19,
+//     taille : '1,70m',
+
+// }
+
+// console.log(moha);
+
+//EXO 2
+
+let johnny = {
+    nom : 'jones',
+    prenom : 'johnny',
+    age : 29,
+    taille : '1,80m',
 
 }
 
-console.log(moha);
+console.log(johnny);
+
+let franklin = {
+
+}
+
+console.log(franklin);
